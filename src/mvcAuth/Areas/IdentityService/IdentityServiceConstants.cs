fd22dec940049036c9bacabea4cb9445e621e94a
Glauber Gasparotto <glauberj@ciandt.com>
2017-06-14 13:59:50 -0300

    netcoreauth

---
+++ src/mvcAuth/Areas/IdentityService/IdentityServiceConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.Service.Extensions;
+
+namespace mvcAuth.Identity
+{
+    // These values are used to setup the identity servcie and should not be changed
+    public class IdentityServiceConstants
+    {
+        // Name of the single identity service tenant
+        public const string Tenant = "IdentityService";
+        
+        // Unique ID of the single identity service tenant
+        public const string TenantId = "6649C7C7-1CC2-4EF5-8C51-1392270599D7";
+
+        // Default policy for the identity service
+        public const string DefaultPolicy = "signinsignup";
+
+        // Identity service version
+        public const string Version = "2.0";
+
+        // Identity service token version
+        public const string TokenVersion = "1.0";
+    }
+}
