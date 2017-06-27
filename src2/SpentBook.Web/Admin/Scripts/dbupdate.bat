975fb9322c0cfa823f19c3d7d955d2065f398416
Glauber Donizeti Gasparotto Junior <juniorgasparotto@msn.com>
2017-06-27 17:53:44 -0300

    dotnetcore

--- src2/SpentBook.Web/Admin/Scripts/dbupdate.bat
+++ src2/SpentBook.Web/Admin/Scripts/dbupdate.bat
@@ -1,4 +1,4 @@
 set now=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
-dotnet ef migrations add %now%
+dotnet ef migrations add %now% -c ApplicationContext
 dotnet run --admin clean-empty-migrations-files
-dotnet ef database update
+dotnet ef database update -c ApplicationContext
--- src2/SpentBook.Web/Admin/Scripts/dropdb.bat
+++ src2/SpentBook.Web/Admin/Scripts/dropdb.bat
@@ -4,7 +4,7 @@ setlocal
 SET /P AREYOUSURE="Are you sure (y/[n])? "
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
-dotnet ef database drop -f
+dotnet ef database drop -f -c ApplicationContext
 rd /s /q "Migrations"
 timeout 10
 :END
