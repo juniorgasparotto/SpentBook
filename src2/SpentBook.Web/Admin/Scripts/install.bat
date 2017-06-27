960061c7d6e2a3b43e5b5229668def149d04e506
Glauber Donizeti Gasparotto Junior <juniorgasparotto@msn.com>
2017-06-27 16:50:17 -0300

    dotnetcore

--- src2/SpentBook.Web/Admin/Scripts/install.bat
+++ src2/SpentBook.Web/Admin/Scripts/install.bat
@@ -39,14 +39,14 @@ dotnet restore
 
 echo.
 echo *** Deleting database if exists...
-dotnet ef database drop -f
+dotnet ef database drop -f --context ApplicationContext
 
 echo.
 echo *** Creating database...
 set now=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
-dotnet ef migrations add %now%
+dotnet ef migrations add %now% --context ApplicationContext
 dotnet run clean-empty-migrations-files
-dotnet ef database update
+dotnet ef database update --context ApplicationContext
 
 echo.
 echo *** DONE!
